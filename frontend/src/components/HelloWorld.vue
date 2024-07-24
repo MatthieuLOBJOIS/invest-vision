84225d6201d62453eb84c32e4d4d60ea20316e6e
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-07-24 21:08:06 +0200

    clear components folder default

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -5,16 +5,14 @@ defineProps({
     required: true
   }
 })
+
+console.log('Hello world')
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h3>Get crypto data test ==></h3>
   </div>
 </template>
 
